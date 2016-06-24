bf0b1ec88e7d946ed836ec5bfcf63287d0b20250
markliujianfeng <markliujianfeng@gmail.com>
2016-06-24 15:33:11 +0800

    Tower of Hanoi

--- Basic/recursive.py
+++ Basic/recursive.py
@@ -12,16 +12,21 @@ def fact_iter(num,product):
         return product;
     return fact_iter(num-1,num*product);
 
-#Recursive 3
+#Recursive 3 Advanced
 
 
 #Test About Tower of Hanoi
 def move(n,a,b,c):
     if n==1:
         print(a+"-->"+c);
+    else:
+        move(n-1,a,c,b);
+        move(1,a,b,c);
+        move(n-1,b,a,c);
+#Step1: take the n-1 pieces from A to B through C;
+#Strp2: take the rest one piece form A to C directly;
+#Step3: take the n-1 pieces from B to C through A.
 
-
-        
 print(fact(5));
 print(fact_1(100));
-move(1,'A','B','c');
+move(3,'A','B','c');

8ae2ae59c769a5d7b9e214e511454ca1119f8883
markliujianfeng <markliujianfeng@gmail.com>
2016-06-24 16:31:11 +0800

    advanced

---
+++ Basic/advanced_perposity.py
@@ -0,0 +1,18 @@
+#usage of append()
+L=[]
+n=1
+while n<=99:
+    L.append(n)
+    n+=2;
+print(L)
+
+#usage of range()
+r=[]
+for i in range(10):
+    r.append(L[i]);
+print(r)
+
+#usageof split
+m=[]
+m=L[0:3]
+print(m);
